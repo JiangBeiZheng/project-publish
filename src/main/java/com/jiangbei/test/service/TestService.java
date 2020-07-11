83c239edb79c334b1894c12b7976de8bad924fb8
JIANGBEI <813476318@qq.com>
2020-07-11 12:46:01 +0800

    测试用service接口

---
+++ src/main/java/com/jiangbei/test/service/TestService.java
@@ -0,0 +1,10 @@
+package com.jiangbei.test.service;
+
+/**
+ * @ AUTHOR: JIANGBEI
+ * @ DES:
+ * @ CREAT TIME:2020/7/11 9:18
+ */
+public interface TestService {
+    public void test(String in);
+}
